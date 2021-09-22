2fcbd0c8b0e1ea9ef93f4674593b9c666441ad7b
Matt Kane <m@mk.gg>
2021-09-22 17:06:49 +0100

    Update demo

--- netlify/functions/stream/stream.ts
+++ netlify/functions/stream/stream.ts
@@ -1,4 +1,5 @@
 import { streamer } from "@netlify/functions";
+import { HTML, updateBlock } from "./templates";
 
 function pause(count: number) {
     return new Promise(resolve => setTimeout(resolve, count))
@@ -8,9 +9,15 @@ export const handler = streamer(async (event, res) => {
     console.log("incoming")
     console.log("event", event)
     res.setHeader("x-test", "value")
-    res.setHeader("content-type", "text/plain")
-    res.write("first block")
+    res.setHeader("content-type", "text/html")
     await pause(1000)
-    res.write("second block")
+
+    res.write(HTML)
+    await pause(1000)
+    res.write(updateBlock(`<p>Well hello there</p>`))
+    await pause(1000)
+    res.write(updateBlock(`<p>Still watching?</p>`))
+    await pause(1000)
+    res.write(updateBlock(`<p><strong>And we're done</strong></p>`))
     res.end()
 })
