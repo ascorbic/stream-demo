ac0f36f714de69bff8ab9be5c2c8d9abad2cb835
Matt Kane <m@mk.gg>
2021-09-22 13:47:01 +0100

    Update

--- netlify/functions/stream/stream.ts
+++ netlify/functions/stream/stream.ts
@@ -7,11 +7,11 @@ function pause(count: number) {
 export const handler = streamer(async (event, res) => {
     res.setHeader("x-test", "value")
     res.setHeader("content-type", "text/plain")
-    res.send("first block")
+    res.write("first block")
     await pause(1000)
-    res.send("second block")
+    res.write("second block")
     for (let index = 0; index < 10; index++) {
         await pause(1000)
-        res.send(`block ${index + 2}`)
+        res.write(`block ${index + 2}`)
     }
 })
