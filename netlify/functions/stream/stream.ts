6c51499b771a36f6806493d3939cd66b1b937291
Matt Kane <m@mk.gg>
2021-09-22 17:18:59 +0100

    Update demo

--- netlify/functions/stream/stream.ts
+++ netlify/functions/stream/stream.ts
@@ -10,14 +10,23 @@ export const handler = streamer(async (event, res) => {
     console.log("event", event)
     res.setHeader("x-test", "value")
     res.setHeader("content-type", "text/html")
-    await pause(1000)
-
     res.write(HTML)
     await pause(1000)
     res.write(updateBlock(`<p>Well hello there</p>`))
     await pause(1000)
     res.write(updateBlock(`<p>Still watching?</p>`))
+    await pause(1000)    
+    res.write(updateBlock(`<p>Did you know that React streaming is just a load of script tags with replaceChildren?</p>`))
     await pause(1000)
-    res.write(updateBlock(`<p><strong>Finished!</strong></p>`))
+    res.write(updateBlock(`<em>Still pretty cool though, right?</em>`))
+    await pause(1000)
+    res.write(updateBlock(`Let's see how long we can do this for`))
+    await pause(1000)
+    for (let i = 0; i < 20; i++) {
+        res.write(updateBlock(`<em>That's been ${i+6} seconds</em>`))
+        await pause(1000)
+        
+    }
+    res.write(updateBlock(`<p><strong>OK, bored now</strong></p>`))
     res.end()
 })
--- netlify/functions/stream/templates.ts
+++ netlify/functions/stream/templates.ts
@@ -1,5 +1,5 @@
 export const HTML = `<html><head><title>Stream demo</title></head><body><div id="root">Loading</div>`
 
 export const updateBlock = (block: string) => `
-<script>document.getElementById('root').innerHTML = '${block.replace(`'`, `\'`)}'</script>
+<script>document.getElementById('root').innerHTML = \`${block}\`</script>
 `
